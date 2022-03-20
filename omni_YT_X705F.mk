cf69913b6ba1452a81987d655ea979bbf73e0889
Johx22 <johannjose2004@gmail.com>
2022-03-20 15:34:24 +0300

    Push some changes

--- omni_YT_X705F.mk
+++ omni_YT_X705F.mk
@@ -23,16 +23,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from YT-X705F device
-$(call inherit-product, device/lenovo/YT-X705F/device.mk)
+$(call inherit-product, device/lenovo/YT_X705F/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := YT-X705F
-PRODUCT_NAME := omni_YT-X705F
+
+PRODUCT_NAME := omni_YT_X705F
 PRODUCT_BRAND := Lenovo
-PRODUCT_MODEL := Lenovo YT-X705F
 PRODUCT_MANUFACTURER := lenovo
-PRODUCT_RELEASE_NAME := Lenovo Lenovo YT-X705F
